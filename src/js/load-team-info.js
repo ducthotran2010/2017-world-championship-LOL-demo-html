a9ee47907a9cdc0ab8b8464fcf1de0c416ddd031
ducthotran2010 <ducthotran2010@gmail.com>
2017-10-09 13:57:53 +0700

    update GUI

---
+++ src/js/load-team-info.js
@@ -0,0 +1,109 @@
+// <!-- winner playin -->
+$(document).ready(function(){
+  var contenttxt = [
+    ["Winner of Match A",  "Cloud9",           "#", "src/images/teams/C9.png"],
+    ["Winner of Match B",  "Fnatic",           "#", "src/images/teams/FNC.png"],
+    ["Winner of Match C",  "1907 Fenerbahçe",  "#", "src/images/teams/1907F.png"],
+    ["Winner of Match D",  "Team WE",          "#", "src/images/teams/WE.png"],
+  ];
+  for (var i = 0; i < contenttxt.length; i++) {
+    var item = document.createElement("div");
+    item.className = "team";
+    document.getElementById("box-team-winners-playin").appendChild(item);
+
+    var nametagP = document.createElement("p");
+    var contentP = document.createTextNode(contenttxt[i][0]);
+    nametagP.appendChild(contentP);
+    item.appendChild(nametagP);
+
+    var nametagA = document.createElement("a");
+    var contentA = document.createTextNode(contenttxt[i][1]);
+    nametagA.appendChild(contentA);
+    nametagA.href = contenttxt[i][2];
+    item.appendChild(nametagA);
+
+    var nametagImg = document.createElement("img");
+    nametagImg.src = contenttxt[i][3];
+    item.appendChild(nametagImg);
+  }
+});
+// <!-- play participants -->
+$(document).ready(function(){
+  var contenttxt = [
+    ["#3 Seed from LPL",    "Team WE",          "#", "src/images/teams/WE.png"],
+    ["#3 Seed from NACLS",  "Cloud9",           "#", "src/images/teams/C9.png"],
+    ["#3 Seed from EULCS",  "Fnatic",           "#", "src/images/teams/FNC.png"],
+    ["#3 Seed from LMS",    "HK Attitude",      "#", "src/images/teams/HKA.png"],
+    ["Winner of CBLoL",     "Team oNe eSports", "#", "src/images/teams/TOE.png"],
+    ["Winner of LJL",       "Rampage",          "#", "src/images/teams/RMP.png"],
+    ["Winner of OPL",       "LG Dire Wolves",   "#", "src/images/teams/DW.png"],
+    ["#2 Seed GPL",         "Young Generation", "#", "src/images/teams/YG.png"],
+    ["Winner of TCL",       "1907 Fenerbahçe",  "#", "src/images/teams/1907F.png"],
+    ["Winner of LCL",       "Gambit.CIS",       "#", "src/images/teams/G-CIS.png"],
+    ["Winner of CLS",       "Kaos Latin Gamers","#", "src/images/teams/KLG.png"],
+    ["Winner of LLN",       "Lyon Gaming",      "#", "src/images/teams/LG.png"],
+  ];
+
+  for (var i = 0; i < contenttxt.length; i++) {
+    var item = document.createElement("div");
+    item.className = "team";
+    document.getElementById("box-team-playin").appendChild(item);
+
+    var nametagP = document.createElement("p");
+    var contentP = document.createTextNode(contenttxt[i][0]);
+    nametagP.appendChild(contentP);
+    item.appendChild(nametagP);
+
+    var nametagA = document.createElement("a");
+    var contentA = document.createTextNode(contenttxt[i][1]);
+    nametagA.appendChild(contentA);
+    nametagA.href = contenttxt[i][2];
+    item.appendChild(nametagA);
+
+    var nametagImg = document.createElement("img");
+    nametagImg.src = contenttxt[i][3];
+    item.appendChild(nametagImg);
+  }
+});
+
+// <!-- THAM GIA GROUP  -->
+$(document).ready(function(){
+  var contenttxt = [
+    ["#1 Seed from LCK",   "Longzhu Gaming",   "#", "src/images/teams/LZ.png"],
+    ["#1 Seed from LPL",   "EDward Gaming",    "#", "src/images/teams/EDG.png"],
+    ["#1 Seed from EULCS", "G2 Esports",       "#", "src/images/teams/G2.png"],
+    ["#1 Seed from LMS",   "Flash Wolves",     "#", "src/images/teams/FW.png"],
+    ["#2 Seed from LCK",   "SK Telecom T1",    "#", "src/images/teams/SKT.png"],
+    ["#3 Seed from LCK",   "Samsung Galaxy",   "#", "src/images/teams/SG.png"],
+    ["#2 Seed from LPL",   "Royal Never Give Up","#","src/images/teams/RNG.png"],
+    ["#1 Seed from NALCS", "Team Solomid",     "#", "src/images/teams/TSM.png"],
+    ["#2 Seed from NALCS", "Immortals",        "#", "src/images/teams/IMT.png"],
+    ["#2 Seed from EULCS", "Misfits",          "#", "src/images/teams/MF.png"],
+    ["#2 Seed from LMS", "ahq e-Sports Club",  "#", "src/images/teams/AHQ.png"],
+    ["#1 Seed from GPL", "GIGABYTE Marines",  "#", "src/images/teams/GM.png"],
+    ["Winner of Match A",  "Cloud9",           "#", "src/images/teams/C9.png"],
+    ["Winner of Match B",  "Fnatic",           "#", "src/images/teams/FNC.png"],
+    ["Winner of Match C",  "1907 Fenerbahçe",  "#", "src/images/teams/1907F.png"],
+    ["Winner of Match D",  "Team WE",          "#", "src/images/teams/WE.png"],
+  ];
+  for (var i = 0; i < contenttxt.length; i++) {
+    var item = document.createElement("div");
+    item.className = "team";
+    document.getElementById("box-team-group-stage").appendChild(item);
+
+    var nametagP = document.createElement("p");
+    var contentP = document.createTextNode(contenttxt[i][0]);
+    nametagP.appendChild(contentP);
+    item.appendChild(nametagP);
+
+    var nametagA = document.createElement("a");
+    var contentA = document.createTextNode(contenttxt[i][1]);
+    nametagA.appendChild(contentA);
+    nametagA.href = contenttxt[i][2];
+    item.appendChild(nametagA);
+
+    var nametagImg = document.createElement("img");
+    nametagImg.src = contenttxt[i][3];
+    item.appendChild(nametagImg);
+  }
+});
